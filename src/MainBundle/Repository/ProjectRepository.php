0e7b8b4488ea79783bc6d6eb090357d8fd0d3ba5
paullandre <erdan08@yahoo.com>
2018-01-16 22:47:09 +0800

    create SQL

--- src/MainBundle/Repository/ProjectRepository.php
+++ src/MainBundle/Repository/ProjectRepository.php
@@ -18,6 +18,22 @@ class ProjectRepository
 
     public function getAllProjects() 
     {
+        $query = $this->em->createQuery('SELECT p.projectName as project, c.name as company, p.description, p.skillId, s.name '
+                . 'FROM MainBundle:Project p '
+                . 'JOIN MainBundle:Company c WITH p.companyId = c.id '
+                . 'JOIN MainBundle:Skills s WITH s.id IN (SELECT x.skillId FROM MainBundle:Project x)');
+        
+        print_r($query->getSQL());
+        die;
+        
+        //->setParameter('user', $user->getId())
+        $result = $query->getResult();
+        
+        print "<pre>";
+        print_r($result);
+        print "</pre>";
+        die;
+        
         $repo  = $this->em->getRepository(Project::class);
         $array = $repo->findAll();
         
